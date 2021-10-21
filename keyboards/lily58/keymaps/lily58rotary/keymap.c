973cd75b38920d18dd8a195c8d9a6760c5658088
Nestor Fuenzalida <nestor.fuenzalida@outlook.com>
2021-10-21 17:27:47 -0300

    Lily Keymap Update

--- keyboards/lily58/keymaps/lily58rotary/keymap.c
+++ keyboards/lily58/keymaps/lily58rotary/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV, \
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_MINS, \
-  KC_LCTRL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
+   KC_LCTL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
    KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,  KC_DEL,   KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RALT, \
                           KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC,  KC_ENT, MO(_RAISE), KC_BSPC, KC_APP \
 ),
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
     KC_ESC, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_NLCK, \
-    KC_TAB, _______,   KC_P7,   KC_P8,   KC_P9, _______,                   _______, _______, _______, _______, _______, KC_PIPE, \
-  KC_LCTRL, _______,   KC_P4,   KC_P5,   KC_P6, _______,                   KC_ASTR, KC_SLSH, KC_LPRN, KC_RPRN, KC_CIRC, KC_AMPR, \
-   KC_LSFT,   KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, KC_MPLY,  KC_DEL, KC_PLUS, KC_MINS, KC_LCBR, KC_RCBR, KC_PERC, KC_RALT, \
+    KC_TAB, _______,   KC_P7,   KC_P8,   KC_P9, _______,                   S(KC_3), KC_PERC,  KC_GRV,S(KC_GRV),S(KC_1), KC_PIPE, \
+   KC_LCTL, _______,   KC_P4,   KC_P5,   KC_P6, _______,                   KC_ASTR, KC_SLSH, KC_LPRN, KC_RPRN, KC_CIRC, KC_AMPR, \
+   KC_LSFT,   KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, KC_MPLY,  KC_DEL, KC_PLUS, KC_MINS, KC_LCBR, KC_RCBR,  KC_EQL, KC_RALT, \
                               KC_LALT, KC_LGUI, _______,  KC_SPC,  KC_ENT, _______, KC_BSPC, KC_APP\
 ),
 /* RAISE
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-    KC_ESC, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, KC_CAPS, \
-    KC_TAB,   KC_F1,   KC_F2,   KC_F3,   KC_F4, _______,                     KC_PGUP, KC_HOME,   KC_UP,  KC_END, _______, _______, \
-  KC_LCTRL,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSLS, _______, \
+    KC_ESC, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
+    KC_TAB,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_ESC,                     KC_PGUP, KC_HOME,   KC_UP,  KC_END, _______,  KC_DEL, \
+   KC_LCTL,   KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSLS, _______, \
    KC_LSFT,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_PSCR,  KC_DEL,  KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_EQL, KC_RALT, \
                               KC_LALT, KC_LGUI, _______,   KC_SPC,  KC_ENT,  _______, KC_BSPC, KC_APP \
 ),
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_ADJUST] = LAYOUT( \
   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   RESET,                   EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  RGB_TOG,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_CAPS, \
+  RGB_MOD,  RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK, \
   RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, G(KC_L), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, G(KC_R), KC_CALC,\
                               KC_LALT, KC_LGUI, _______,  KC_SPC,  KC_ENT, _______, KC_BSPC, KC_APP \
   )
